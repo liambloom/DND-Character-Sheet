a7e409ce89d50a671814bdbaed9ed1e1953ae0be
liambloom <liamrbloom@gmail.com>
2024-01-14 01:38:04 -0500

    Added db schema dump

---
+++ db.sql
@@ -0,0 +1,81 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 16.1
+-- Dumped by pg_dump version 16.1
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- Name: citext; Type: EXTENSION; Schema: -; Owner: -
+--
+
+CREATE EXTENSION IF NOT EXISTS citext WITH SCHEMA public;
+
+
+--
+-- Name: EXTENSION citext; Type: COMMENT; Schema: -; Owner: 
+--
+
+COMMENT ON EXTENSION citext IS 'data type for case-insensitive character strings';
+
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- Name: users; Type: TABLE; Schema: public; Owner: liamr
+--
+
+CREATE TABLE public.users (
+    user_id uuid NOT NULL,
+    username character varying(50) NOT NULL,
+    email public.citext NOT NULL,
+    display_name character varying(80) NOT NULL,
+    password bit(32)[] NOT NULL,
+    salt bit(16)[] NOT NULL,
+    created timestamp without time zone NOT NULL
+);
+
+
+ALTER TABLE public.users OWNER TO liamr;
+
+--
+-- Name: users users_email_key; Type: CONSTRAINT; Schema: public; Owner: liamr
+--
+
+ALTER TABLE ONLY public.users
+    ADD CONSTRAINT users_email_key UNIQUE (email);
+
+
+--
+-- Name: users users_pkey; Type: CONSTRAINT; Schema: public; Owner: liamr
+--
+
+ALTER TABLE ONLY public.users
+    ADD CONSTRAINT users_pkey PRIMARY KEY (user_id);
+
+
+--
+-- Name: users users_username_key; Type: CONSTRAINT; Schema: public; Owner: liamr
+--
+
+ALTER TABLE ONLY public.users
+    ADD CONSTRAINT users_username_key UNIQUE (username);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
