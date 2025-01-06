748151a54a871b36780a46dd21deac6c1bea2a78
liambloom <liamrbloom@gmail.com>
2025-01-06 18:20:13 -0500

    Fixed? font resizing

--- static/scripts/homepage.js
+++ static/scripts/homepage.js
@@ -1,24 +1,39 @@
-fetch("/api/user/amILoggedIn")
-    .then(res => res.json())
-    .then(res => {
+const DOMLoadPromise = new Promise((res, rej) => {
+    if (document.readyState === "loading") {
+        window.addEventListener("DOMContentLoaded", () => res());
+    }
+    else {
+        res();
+    }
+});
+
+const loggedIn = fetch("/api/user/amILoggedIn")
+    .then(res => res.json());
+
+Promise.all([loggedIn, DOMLoadPromise])
+    .then(([res]) => {
         if (res) {
             document.getElementById("signup").style.display = "none";
             document.getElementById("login").innerText = "Go to my character sheets";
         }
     });
-console.log("foo");
-const frontTitleBox = document.getElementById("front-title-container");
-const backTitle = document.getElementById("back-tagline")
-const frontTitle = document.getElementById("tagline")
-function fixTitleSize() {
-    let i;
-    for (i = 100; i > 0; i--) {
-        frontTitle.style.setProperty("font-size", `${i}px`);
-        if (frontTitleBox.scrollHeight === frontTitleBox.clientHeight) {
-            break;
+
+DOMLoadPromise.then(() => {
+    const frontTitleBox = document.getElementById("front-title-container");
+    const backTitle = document.getElementById("back-tagline")
+    const frontTitle = document.getElementById("tagline")
+    function fixTitleSize() {
+        let i;
+        for (i = 100; i > 0; i--) {
+            frontTitle.style.setProperty("font-size", `${i}px`);
+            if (frontTitleBox.scrollHeight === frontTitleBox.clientHeight) {
+                break;
+            }
         }
+        backTitle.style.setProperty("font-size", `${i}px`);
+    
     }
-    backTitle.style.setProperty("font-size", `${i}px`);
-}
-fixTitleSize();
-window.addEventListener("resize", () => fixTitleSize());
+    fixTitleSize();
+    document.fonts.ready.then(fixTitleSize);
+    window.addEventListener("resize", fixTitleSize);
+});
